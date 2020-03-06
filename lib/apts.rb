749523cc9dfd1428e485373806bd37205680e8d0
Juan Li Puma <juanlipuma94@gmail.com>
2020-03-06 20:25:30 -0300

    Make everything "static" to run main

--- lib/apts.rb
+++ lib/apts.rb
@@ -13,44 +13,48 @@ require 'apts/version'
 module Apts
   class Error < StandardError; end
 
-  def get_history
-    IO.readlines('seen.txt', chomp: true)
-  rescue StandardError
-    []
-  end
+  class Main
+    class << self
+      def get_history
+        IO.readlines('seen.txt', chomp: true)
+      rescue StandardError
+        []
+      end
 
-  def notify(posting)
-    token = '1014765302:AAG2r4z0sNYCsUB_nKmSVcl71picZDA2gwA'
-    chat_id = '332592625'
-    open "https://api.telegram.org/bot#{token}/sendMessage?chat_id=#{chat_id}&text=#{posting[:url]}"
-  end
+      def notify(posting)
+        token = '1014765302:AAG2r4z0sNYCsUB_nKmSVcl71picZDA2gwA'
+        chat_id = '332592625'
+        open "https://api.telegram.org/bot#{token}/sendMessage?chat_id=#{chat_id}&text=#{posting[:url]}"
+      end
 
-  def mark_as_seen(unseen_postings)
-    IO.write('seen.txt', unseen_postings.map { |u| u[:id] }.join("\n"), mode: 'a')
-  end
+      def mark_as_seen(unseen_postings)
+        IO.write('seen.txt', unseen_postings.map { |u| u[:id] }.join("\n"), mode: 'a')
+      end
 
-  @parsers = [
-    Apts::Parser.new('https://www.zonaprop.com.ar', 'a.go-to-posting'),
-    Apts::Parser.new('https://www.argenprop.com', 'div.listing__items div.listing__item a'),
-    Apts::Parser.new('https://inmuebles.mercadolibre.com.ar', 'li.results-item .rowItem.item a')
-  ]
+      def run
+        @history = get_history
+        @parsers = [
+          Apts::Parser.new('https://www.zonaprop.com.ar', 'a.go-to-posting'),
+          Apts::Parser.new('https://www.argenprop.com', 'div.listing__items div.listing__item a'),
+          Apts::Parser.new('https://inmuebles.mercadolibre.com.ar', 'li.results-item .rowItem.item a')
+        ]
+        @urls = %w[
+          https://www.zonaprop.com.ar/departamentos-alquiler-capital-federal-2-ambientes-menos-30000-pesos-orden-publicado-descendente.html
+          https://www.argenprop.com/departamento-alquiler-localidad-capital-federal-2-ambientes-hasta-30000-pesos-orden-masnuevos
+        ].map { |u| URI(u) }
 
-  @urls = %w[
-    https://www.zonaprop.com.ar/departamentos-alquiler-capital-federal-2-ambientes-menos-30000-pesos-orden-publicado-descendente.html
-    https://www.argenprop.com/departamento-alquiler-localidad-capital-federal-2-ambientes-hasta-30000-pesos-orden-masnuevos
-  ].map { |u| URI(u) }
+        @urls.each do |url|
+          (parser = @parsers.find { |p| p.url.host == url.host }) || raise("No parser found for #{url}")
+          html = Nokogiri::HTML(open(url))
+          postings = parser.extract_postings html
+          seen, unseen = postings.partition { |l| @history.include? l[:id] }
 
-  @history = get_history
+          puts "#{seen.length} seen, #{unseen.length} unseen"
 
-  @urls.each do |url|
-    (parser = @parsers.find { |p| p.url.host == url.host }) || raise("No parser found for #{url}")
-    html = Nokogiri::HTML(open(url))
-    postings = parser.extract_postings html
-    seen, unseen = postings.partition { |l| @history.include? l[:id] }
-
-    puts "#{seen.length} seen, #{unseen.length} unseen"
-
-    unseen.each { |u| notify u }
-    mark_as_seen unseen
+          unseen.each { |u| notify u }
+          mark_as_seen unseen
+        end
+      end
+    end
   end
 end
