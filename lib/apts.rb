d74355b25cb60c3d58e12cbad5af09fe8d875ecd
Juan Li Puma <juanlipuma94@gmail.com>
2020-03-06 21:23:29 -0300

    Add logger, fix history newlines

--- lib/apts.rb
+++ lib/apts.rb
@@ -4,6 +4,7 @@ require 'open-uri'
 require 'nokogiri'
 require 'digest/sha1'
 require 'uri'
+require 'logger'
 
 require 'apts/parser'
 require 'apts/version'
@@ -15,8 +16,8 @@ module Apts
 
   class Main
     class << self
-      def get_history
-        IO.readlines('seen.txt', chomp: true)
+      def get_history(history_file)
+        IO.readlines(history_file, chomp: true)
       rescue StandardError
         []
       end
@@ -27,12 +28,20 @@ module Apts
         open "https://api.telegram.org/bot#{token}/sendMessage?chat_id=#{chat_id}&text=#{posting[:url]}"
       end
 
-      def mark_as_seen(unseen_postings)
-        IO.write('seen.txt', unseen_postings.map { |u| u[:id] }.join("\n"), mode: 'a')
+      def mark_as_seen(unseen_postings, file)
+        return if unseen_postings.empty?
+
+        content = unseen_postings.map { |u| u[:id] }.join("\n") << "\n"
+        file.write content
+        file.flush
       end
 
       def run
-        @history = get_history
+        logger = Logger.new STDOUT
+
+        @history_file = File.open 'seen.txt', 'a+'
+        @history = get_history @history_file
+        logger.debug "Loaded #{@history.length} seen postings"
         @parsers = [
           Apts::Parser.new('https://www.zonaprop.com.ar', 'a.go-to-posting'),
           Apts::Parser.new('https://www.argenprop.com', 'div.listing__items div.listing__item a'),
@@ -43,17 +52,24 @@ module Apts
           https://www.argenprop.com/departamento-alquiler-localidad-capital-federal-2-ambientes-hasta-30000-pesos-orden-masnuevos
         ].map { |u| URI(u) }
 
-        @urls.each do |url|
+        logger.debug "#{@urls.length} URLs configured"
+
+        @urls.each_with_index do |url, i|
+          logger.info "Extracting postings from #{url.host}..."
           (parser = @parsers.find { |p| p.url.host == url.host }) || raise("No parser found for #{url}")
           html = Nokogiri::HTML(open(url))
           postings = parser.extract_postings html
+          logger.debug "Extracted #{postings.length} postings"
           seen, unseen = postings.partition { |l| @history.include? l[:id] }
 
-          puts "#{seen.length} seen, #{unseen.length} unseen"
-
+          logger.info "Postings: #{seen.length} seen, #{unseen.length} unseen"
+          logger.info 'Notifying unseen postings...'
           unseen.each { |u| notify u }
-          mark_as_seen unseen
+          logger.info 'Marking unseens as seen'
+          mark_as_seen unseen, @history_file
         end
+
+        logger.info "DONE"
       end
     end
   end
