d99da591c5d7ab4ca4da240e869cc469c78cc9f4
Juan Li Puma <juanlipuma94@gmail.com>
2020-05-18 22:57:57 -0300

    Allow running multiple parsers of the same host while avoiding duplicates

--- lib/apts.rb
+++ lib/apts.rb
@@ -26,51 +26,79 @@ module Apts
 
       def run
         Dotenv.load '.env'
-        logger = Logger.new STDOUT
+        @logger = Logger.new STDOUT
 
         @blacklist = Apts::Blacklist.new
         @drive = Apts::Google::Drive.new
         @history = @drive.seen
-        logger.debug "Loaded #{@history.length} seen listings"
+        @logger.debug "Loaded #{@history.length} seen listings"
 
         @parsers = [
+          # ZonaProp <= 5 años
+          Apts::Parsers::ZonaPropParser.new(URI('https://www.zonaprop.com.ar/departamentos-alquiler-palermo-belgrano-recoleta-barrio-norte-las-canitas-nunez-villa-crespo-colegiales-2-ambientes-hasta-5-anos-menos-30000-pesos-orden-publicado-descendente.html')),
+          # ZonaProp 5 años <= x <= 10 años
+          Apts::Parsers::ZonaPropParser.new(URI('https://www.zonaprop.com.ar/departamentos-alquiler-palermo-belgrano-recoleta-barrio-norte-las-canitas-nunez-villa-crespo-colegiales-2-ambientes-entre-5-y-10-anos-menos-30000-pesos-orden-publicado-descendente.html')),
+          # ZonaProp 10 años <= x <= 20 años
+          Apts::Parsers::ZonaPropParser.new(URI('https://www.zonaprop.com.ar/departamentos-alquiler-palermo-belgrano-recoleta-barrio-norte-las-canitas-nunez-villa-crespo-colegiales-2-ambientes-entre-10-y-20-anos-menos-30000-pesos-orden-publicado-descendente.html')),
+          # ZonaProp sin filtro de antigüedad
           Apts::Parsers::ZonaPropParser.new(URI('https://www.zonaprop.com.ar/departamentos-alquiler-palermo-belgrano-recoleta-barrio-norte-las-canitas-nunez-villa-crespo-colegiales-2-ambientes-menos-30000-pesos-orden-publicado-descendente.html')),
+
           # THE TRAILING / FOR ML IS NECESSARY!
           Apts::Parsers::MercadoLibreParser.new(URI('https://inmuebles.mercadolibre.com.ar/departamentos/alquiler/2-ambientes/capital-federal/barrio-norte-o-belgrano-o-belgrano-barrancas-o-belgrano-c-o-belgrano-chico-o-belgrano-r-o-botanico-o-colegiales-o-las-canitas-o-nunez-o-palermo-o-palermo-chico-o-palermo-hollywood-o-palermo-nuevo-o-palermo-soho-o-palermo-viejo-o-recoleta-o-villa-crespo/')),
+
           Apts::Parsers::ArgenPropParser.new(URI('https://www.argenprop.com/departamento-alquiler-barrio-br-norte-barrio-belgrano-barrio-palermo-barrio-colegiales-barrio-nunez-barrio-villa-crespo-2-ambientes-hasta-30000-pesos-orden-masnuevos')),
         ]
-        logger.debug "#{@parsers.length} parsers configured"
+        @logger.debug "#{@parsers.length} parsers configured"
 
-        listings = []
-        max_listings_per_parser = ENV['MAX_LISTINGS_PER_PARSER']&.to_i
-
+        # Group listings by host
+        grouped_listings = Hash.new { |hash, key| hash[key] = [] }
         @parsers.each do |parser|
-          parser_listings = parser.extract_listings
-          # Apply blacklist
-          original_size = parser_listings.length
-          parser_listings.reject! { |l| @blacklist.blacklisted? l }
-          filtered_listings = original_size - parser_listings.length
-          logger.info "Filtered out #{filtered_listings} blacklisted listings" if filtered_listings.positive?
-
-          # Partition by seen
-          seen, unseen = parser_listings.partition { |l| @history.include? l.id }
-
-          logger.info "Extracted #{parser_listings.length} listings from #{parser.url.host}: #{seen.length} seen, #{unseen.length} unseen"
-          unless max_listings_per_parser.nil?
-            logger.debug "Limiting to #{max_listings_per_parser} listings"
-            unseen = unseen.take max_listings_per_parser
-          end
-          listings.concat unseen
+          unseen_listings = get_unseen_listings(parser)
+          grouped_listings[parser.url.host].concat unseen_listings
+          # Add to history so other runs of the same parser don't count
+          @history.concat unseen_listings.map(&:id)
         end
 
-        logger.info "Extracted #{listings.length} unseen listings in total"
-        logger.info "Average score: #{format '%.0f', listings.map(&:score).sum / listings.length}" if listings.any?
-        logger.info 'Notifying unseen listings...'
+        listings = post_process(grouped_listings).values.flatten
+        @logger.info "Extracted #{listings.length} unseen listings in total"
+        @logger.info "Average score: #{format '%.0f', listings.map(&:score).sum / listings.length}" if listings.any?
+
+        @logger.info 'Notifying unseen listings...'
         listings.each { |u| notify u }
-        logger.info 'Marking unseens as seen...'
+
+        @logger.info 'Marking unseens as seen...'
         @drive.mark_as_seen listings
 
-        logger.info 'DONE'
+        @logger.info 'DONE'
+      end
+
+      def get_unseen_listings(parser)
+        parser_listings = parser.extract_listings
+        # Apply blacklist
+        original_size = parser_listings.length
+        parser_listings.reject! { |l| @blacklist.blacklisted? l }
+        filtered_listings = original_size - parser_listings.length
+        @logger.info "Filtered out #{filtered_listings} blacklisted listings" if filtered_listings.positive?
+
+        # Partition by seen
+        seen, unseen = parser_listings.partition { |l| @history.include? l.id }
+
+        @logger.info "Extracted #{parser_listings.length} listings from #{parser.url.host}: #{seen.length} seen, #{unseen.length} unseen"
+        unseen
+      end
+
+      # Remove duplicate entries limit results in each group
+      def post_process(grouped_listings)
+        max_listings_per_parser = ENV['MAX_LISTINGS_PER_PARSER']&.to_i
+        @logger.debug "Limiting to #{max_listings_per_parser} listings per host" unless max_listings_per_parser.nil?
+        grouped_listings.transform_values do |entries|
+          # Remove duplicates in each group
+          result = entries.uniq
+          # Limit results if configured to do so
+          result = entries.take max_listings_per_parser unless max_listings_per_parser.nil?
+
+          result
+        end
       end
     end
   end
